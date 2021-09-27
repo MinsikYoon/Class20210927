b1fe8b45755ed28194e48c95f96dbec9d85c5fba
Minsik Yoon <alstlr9784@gmail.com>
2021-09-27 10:39:58 +0900

    main.cpp 함수 추가

--- Class0927/main.cpp
+++ Class0927/main.cpp
@@ -1,14 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main() {
+void PrintInteger(int n) {
 
-	printf("Hello world!\n");
-	printf("Hello world!\n");
-	printf("Hello world!\n");
-	printf("Hello world!\n");
-	printf("Hello world!\n");
+	for (int i = 0; i < n; i++) {
+		printf("Hello : %d\n", i);
+	}
 
 }
+int main() {
+
+	PrintInteger(20);
+
+}
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//
+//	printf("Hello world!\n");
+//	printf("Hello world!\n");
+//	printf("Hello world!\n");
+//	printf("Hello world!\n");
+//	printf("Hello world!\n");
+//
+//}
 
 //#include<stdio.h>
 //#include<stdlib.h>
